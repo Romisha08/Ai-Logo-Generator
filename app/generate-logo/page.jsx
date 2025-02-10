814d356278c307ec142e4fee98230a6549e753c1
Romisha08 <153627362+Romisha08@users.noreply.github.com>
2025-02-10 13:26:06 +0530

    Update page.jsx

--- app/generate-logo/page.jsx
+++ app/generate-logo/page.jsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import { useSearchParams } from "next/navigation";
 
 
+import { toast } from "react-toastify";
 
 function GenerateLogo() {
   const { userDetail, setUserDetail } = useContext(UserDetailContex);
@@ -34,7 +35,7 @@ function GenerateLogo() {
   useEffect(()=>{
     if(typeof window !=undefined &&logoImage)
     {
-      localStorafe.clear();
+      localStorage.clear();
     }
   },[logoImage])
 
